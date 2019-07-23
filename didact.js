6bdc4fb92309d00fe2074b9c972f63a8e9da7205
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 13:04:50 +0200

    Add new fibers to the fiber tree

--- didact.js
+++ didact.js
@@ -84,6 +84,15 @@ function performUnitOfWork(fiber) {
       parent: fiber,
       dom: null,
     }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
   }
 }
 
