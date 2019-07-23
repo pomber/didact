d5d0f4aa4a137735dcfba1bc14787cc85bab6d30
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 12:58:29 +0200

    Set element props

--- didact.js
+++ didact.js
@@ -28,6 +28,13 @@ function render(element, container) {
       ? document.createTextNode("")
       : document.createElement(element.type)
 
+  const isProperty = key => key !== "children"
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    })
+
   element.props.children.forEach(child =>
     render(child, dom)
   )
