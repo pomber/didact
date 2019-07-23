4ee185f2ded57714345ba8b8d7f7d1551f96b7b4
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 12:57:39 +0200

    Append children

--- didact.js
+++ didact.js
@@ -25,6 +25,10 @@ function createTextElement(text) {
 function render(element, container) {
   const dom = document.createElement(element.type)
 
+  element.props.children.forEach(child =>
+    render(child, dom)
+  )
+
   container.appendChild(dom)
 }
 
