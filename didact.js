ae165f12df7755f90542a70306fa3820e824bce7
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 12:53:43 +0200

    Replace createElement

--- didact.js
+++ didact.js
@@ -1,3 +1,13 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children,
+    },
+  }
+}
+
 const element = React.createElement(
   "div",
   { id: "foo" },
