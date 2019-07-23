d44d7cbf4b44813e50a51da9afa356e09883f5b1
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 12:54:03 +0200

    Handle text elements

--- didact.js
+++ didact.js
@@ -3,7 +3,21 @@ function createElement(type, props, ...children) {
     type,
     props: {
       ...props,
-      children,
+      children: children.map(child =>
+        typeof child === "object"
+          ? child
+          : createTextElement(child)
+      ),
+    },
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
     },
   }
 }
