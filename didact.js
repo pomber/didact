c2bae275277bad915d0c8889fb326a3b3177940a
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 12:52:34 +0200

    Transpile createElement

--- didact.js
+++ didact.js
@@ -1,8 +1,8 @@
-const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
-  </div>
+const element = React.createElement(
+  "div",
+  { id: "foo" },
+  React.createElement("a", null, "bar"),
+  React.createElement("b")
 )
 const container = document.getElementById("root")
 ReactDOM.render(element, container)
