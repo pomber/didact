61adbae0f40fe5f27b905161c096cbaa4a3291c7
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 13:15:55 +0200

    Set wipRoot

--- didact.js
+++ didact.js
@@ -38,16 +38,22 @@ function createDom(fiber) {
   return dom
 }
 
+function commitRoot() {
+  // TODO
+}
+
 function render(element, container) {
-  nextUnitOfWork = {
+  wipRoot = {
     dom: container,
     props: {
       children: [element],
     },
   }
+  nextUnitOfWork = wipRoot
 }
 
 let nextUnitOfWork = null
+let wipRoot = null
 
 function workLoop(deadline) {
   let shouldYield = false
@@ -57,6 +63,11 @@ function workLoop(deadline) {
     )
     shouldYield = deadline.timeRemaining() < 1
   }
+
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot()
+  }
+
   requestIdleCallback(workLoop)
 }
 
