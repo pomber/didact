301b22c7d6b5fdb7b9ccc552cbdb63acdd6dc530
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 13:38:02 +0200

    Use JSX again

--- didact.js
+++ didact.js
@@ -244,15 +244,8 @@ const Didact = {
 
 /** @jsx Didact.createElement */
 function App({ props }) {
-  return Didact.createElement(
-    "h1",
-    null,
-    "Hi ",
-    props.name
-  )
+  return <h1>Hi {props.name}</h1>
 }
-const element = Didact.createElement(App, {
-  name: "foo",
-})
+const element = <App name="foo" />
 const container = document.getElementById("root")
 Didact.render(element, container)
