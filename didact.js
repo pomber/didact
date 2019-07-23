93efc2f23699bfbd154d077711508151e2e7aa6c
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 12:55:08 +0200

    Use JSX

--- didact.js
+++ didact.js
@@ -26,11 +26,12 @@ const Didact = {
   createElement,
 }
 
-const element = Didact.createElement(
-  "div",
-  { id: "foo" },
-  Didact.createElement("a", null, "bar"),
-  Didact.createElement("b")
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
 )
 const container = document.getElementById("root")
 ReactDOM.render(element, container)
