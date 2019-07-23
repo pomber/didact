e04b2b15b4100e15c4e161095dfae5e92b305610
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 12:58:03 +0200

    Render text elements

--- didact.js
+++ didact.js
@@ -23,7 +23,10 @@ function createTextElement(text) {
 }
 
 function render(element, container) {
-  const dom = document.createElement(element.type)
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type)
 
   element.props.children.forEach(child =>
     render(child, dom)
