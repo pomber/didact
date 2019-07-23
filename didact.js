184cca799f4ce20af50a6e8374ba809713dfa684
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 13:37:06 +0200

    Change example

--- didact.js
+++ didact.js
@@ -243,11 +243,9 @@ const Didact = {
 }
 
 /** @jsx Didact.createElement */
-const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
-  </div>
-)
+function App({ props }) {
+  return <h1>Hi {props.name}</h1>
+}
+const element = <App name="foo" />
 const container = document.getElementById("root")
 Didact.render(element, container)
