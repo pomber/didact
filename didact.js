5a59597664cacc288c461cbff41c7a20e8579cc1
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-23 13:45:35 +0200

    Add useState function

--- didact.js
+++ didact.js
@@ -197,6 +197,10 @@ function updateFunctionComponent(fiber) {
   reconcileChildren(fiber, children)
 }
 
+function useState(initial) {
+  // TODO
+}
+
 function updateHostComponent(fiber) {
   if (!fiber.dom) {
     fiber.dom = createDom(fiber)
